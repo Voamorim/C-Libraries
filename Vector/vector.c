6822851702697cc7cb544cfb0dd24f77711563bb
Voamorim <voamorim345@gmail.com>
2026-07-21 11:56:03 -0300

    fix: changes comments to english

--- Vector/vector.c
+++ Vector/vector.c
@@ -1,12 +1,16 @@
 #include "vector.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-Vector* createVector(const int initial_capacity, const int item_size){
-    Vector* v = (Vector*) malloc (sizeof(Vector));
+Vector* vectorCreate(const unsigned int initial_capacity, const unsigned int item_size) {
+    Vector* v = (Vector*)malloc(sizeof(Vector));
 
-    if(v == NULL){
-        printf("Erro ao alocar memoria para a estrutura vector.\n");
+    if (v == NULL) {
+        perror(
+            "[vector.c][createVector()] ERROR: Failed to allocate memory for the vector "
+            "structure.\n");
         return NULL;
     }
 
@@ -16,8 +20,9 @@ Vector* createVector(const int initial_capacity, const int item_size){
 
     v->data = malloc(initial_capacity * item_size);
 
-    if(v->data == NULL){
-        printf("Erro ao alocar memoria para os dados do vector.\n");
+    if (v->data == NULL) {
+        perror(
+            "[vector.c][createVector()] ERROR: Failed to allocate memory for the vector dada.\n");
         free(v);
         return NULL;
     }
@@ -25,51 +30,80 @@ Vector* createVector(const int initial_capacity, const int item_size){
     return v;
 }
 
-void vectorPushBack(Vector *vector, const void *value){
-    if(vector->size == vector->capacity){
+void vectorPushBack(Vector* vector, const void* value) {
+    if (vector == NULL) {
+        perror("[vector.c][vectorPushBack()] WARNING: Vector object does not exist.\n");
+        return;
+    }
+
+    if (vector->size == vector->capacity) {
         vector->capacity = vector->capacity * 2 > 0 ? vector->capacity * 2 : 1;
         vector->data = realloc(vector->data, vector->capacity * vector->item_size);
 
-        if(vector->data == NULL){
-            printf("Erro ao alocar memoria para os itens do vector.\n");
-            exit(1);
+        if (vector->data == NULL) {
+            perror(
+                "[vector.c][vectorPushBack()] ERROR: Failed to allocate memory for the vector "
+                "items.\n");
+            return;
         }
     }
-    // Calcula o endereço para o qual o novo elemento deve ser copiado
-    char *dest = (char*) vector->data + vector->size * vector->item_size;
+    // Calculates the address to which the new element should be copied
+    char* dest = (char*)vector->data + vector->size * vector->item_size;
     memcpy(dest, value, vector->item_size);
     vector->size += 1;
 }
 
-void* vectorGet(const Vector *vector, int index){
-    if(index >= vector->size){
-        printf("Erro: indice fora dos limites do vector.\n");
-        exit(1);
+void* vectorGet(const Vector* vector, int index) {
+    if (vector == NULL) {
+        perror("[vector.c][vectorGet()] WARNING: Vector object does not exist.\n");
+        return NULL;
     }
-    // Calcula o endereço do elemento desejado
-    return (char*) vector->data + index * vector->item_size;
+
+    if (index >= vector->size) {
+        perror("[vector.c][vectorGet()] ERROR: Vector index out of bounds.\n");
+        return NULL;
+    }
+    // Calculates the desired item address
+    return (char*)vector->data + index * vector->item_size;
 }
 
-void vectorSet(Vector* vector, int index, const void *value){
-    if(index >= vector->size){
-        printf("Erro: indice fora dos limites do vector.\n");
-        exit(1);
+void vectorSet(Vector* vector, int index, const void* value) {
+    if (vector == NULL) {
+        perror("[vector.c][vectorSet()] WARNING: Vector object does not exist.\n");
+        return;
     }
-    // Calcula o endereço do elemento a ser modificado
-    char* dest = (char*) vector->data + index * vector->item_size;
+
+    if (index >= vector->size) {
+        perror("[vector.c][vectorSet()] ERROR: Vector index out of bounds.\n");
+        return;
+    }
+    // Calculates the to be modified item address
+    char* dest = (char*)vector->data + index * vector->item_size;
     memcpy(dest, value, vector->item_size);
 }
 
-int vectorSize(const Vector *vector){
+int vectorSize(const Vector* vector) {
+    if (vector == NULL) {
+        perror("[vector.c][vectorSize()] WARNING: Vector object does not exist.\n");
+        return 0;
+    }
     return vector->size;
 }
 
-int vectorCapacity(const Vector *vector){
+int vectorCapacity(const Vector* vector) {
+    if (vector == NULL) {
+        perror("[vector.c][vectorCapacity()] WARNING: Vector object does not exist.\n");
+        return 0;
+    }
     return vector->capacity;
 }
 
-void freeVector(Vector *vector){
+void vectorFree(Vector* vector) {
+    if (vector == NULL) {
+        perror("[vector.c][vectorFree()] WARNING: Vector object does not exist.\n");
+        return;
+    }
+
     free(vector->data);
     free(vector);
 }
-
--- Vector/vector.h
+++ Vector/vector.h
@@ -1,19 +1,19 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-typedef struct Vector{
+typedef struct Vector {
     void* data;
-    int size;      // Quantidade de elementos atualmente no vector
-    int capacity;  // Capacidade total do vector
-    int item_size; // Quantidade de bytes ocupados por cada item do vector
+    int size;       // Number of elements currently in the vector
+    int capacity;   // Vector total capacity
+    int item_size;  // Number of bytes occupied by each item in the vector
 } Vector;
 
-Vector* createVector(const int initial_capacity, const int item_size);
-void vectorPushBack(Vector *vector, const void* value);
+Vector* vectorCreate(const unsigned int initial_capacity, const unsigned int item_size);
+void vectorPushBack(Vector* vector, const void* value);
 void* vectorGet(const Vector* vector, int index);
 void vectorSet(const Vector* vector, int index, const void* value);
 int vectorSize(const Vector* vector);
 int vectorCapacity(const Vector* vector);
-void freeVector(Vector *vector);
+void vectorFree(Vector* vector);
 
 #endif
